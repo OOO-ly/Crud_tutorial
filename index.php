f9962fa43e2acc12ec8c77efeca8aceb9b29002f
OOO-ly <57537902+OOO-ly@users.noreply.github.com>
2021-01-25 16:50:59 +0900

    tng_day1_php_tutorial_2

--- index.php
+++ index.php
@@ -6,9 +6,38 @@
     <title>Document</title>
 </head>
 <body>
-  <h1>Dynimic Web</h1>
-  <?php 
-  
-  ?>  
+  <h1><a href="index.php">Dynimic Web</a></h1>
+  <ol>
+    <?php 
+        $lang_list = scandir('data');      
+        $i = 0;
+        while($i<count($lang_list)){
+            if($lang_list[$i] != '.'){
+                if($lang_list[$i] != '..'){
+                    ?>
+                    <li><a href="index.php?id=<?=$lang_list[$i]?>"><?=$lang_list[$i]?></a></li>
+                    <?php
+                }
+           }$i++;
+        }
+    ?>
+  </ol>
+<h2>
+<?php
+    if(isset($_GET['id'])){
+        echo $_GET['id'];
+    }else{
+        echo "welcome";
+    }
+?>
+</h2>
+
+<?php
+if(isset($_GET['id'])){
+    echo file_get_contents("data/".$_GET['id']);
+}
+?>
+
+
 </body>
 </html>
---
+++ php_lang.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP_TUTORIAL</h1>
+    <!-- 조건문 -->
+   
+    <h2><a href="./php_lang.php?id=<?=$value?>">
+    값 변경
+    </a>
+    </h2>
+
+
+    <?php
+   
+    if($value != 1){
+        echo " true";
+        $value * -1;
+    }
+    else{
+        echo "false";
+        $value * -1;
+    }
+
+    ?>
+    
+
+    <!-- 반복문 -->
+
+    <hr>
+    <h2>while 반복문</h2>
+    <?php
+        $i = 0;
+        while($i<10)
+        {
+            echo $i."<br>";
+            $i +=3;
+        }
+    ?>
+    <hr>
+    <h2>for 반복문</h2>
+    <?php
+    for($j = 0; $j <= 10; $j+=2)
+    {
+        echo $j."<br>"; 
+    }
+    ?>
+
+    <!-- 배열 -->
+    <h2>배열</h2>
+    <?php
+    
+    $coworkers = array('lee','kim','park','choi');
+
+    $list_ = array();
+    $i = 0;
+    while($i < 10){
+        $list_[$i] = $i; 
+        $i++;
+    } 
+    $j = 0;
+    while($j<count($coworkers)){
+    echo $coworkers[$j]."<br>";
+    $j++;
+    }
+
+    echo count($coworkers)."<br>";
+    echo var_dump($coworkers)."<br>";
+    array_push($coworkers, "go");
+    echo var_dump($coworkers)."<br>";
+    $temp = array_pop($coworkers);
+    echo $temp;
+    echo var_dump($coworkers)."<br>";
+    echo $coworkers[array_search('kim',$coworkers)];
+    ?>
+
+</body>
+</html>
